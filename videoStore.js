1292d857d1288cfc09f6ee371b9cea94317c7d54
sinholee <dltlsgh5@gmail.com>
2021-04-12 16:51:13 +0900

    refactor: amountFor 함수 생성-> switch문 바깥으로 빼기

---
+++ videoStore.js
@@ -0,0 +1,54 @@
+import invoices from "./invoices.js";
+import plays from "./plays.js";
+
+function amountFor(perf, play) {
+	let thisAmount = 0;
+	switch (play.type) {
+		case "tragedy":
+			thisAmount = 40000;
+			if (perf.audience > 30) {
+				thisAmount += 1000 * (perf.audience - 30);
+			}
+			break;
+		case "comedy":
+			thisAmount = 30000;
+			if (perf.audience > 20) {
+				thisAmount += 10000 + 500 * (perf.audience - 20);
+			}
+			thisAmount += 300 * perf.audience;
+			break;
+		default:
+			throw new Error(`알수없는 장르 ${play.type}`);
+			break;
+	}
+	return thisAmount;
+}
+
+function statement(invoice, plays) {
+	let totalAmount = 0;
+	let volumeCredits = 0;
+	let result = `청구 내역 (고객명:${invoice.customer})\n`;
+	const format = new Intl.NumberFormat("en-US", {
+		style: "currency",
+		currency: "USD",
+		minimumFractionDigits: 2,
+	}).format;
+
+	for (let perf of invoice.performances) {
+		const play = plays[perf.playID];
+		let thisAmount = amountFor(perf, play);
+		// 포인트를 적립한다.? 30명 이상이면 포인트를 준다.
+		volumeCredits += Math.max(perf.audience - 30, 0);
+		// 코메디 장르일 경우 5명 마다 추가 포인트를 적립해준다.
+		if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+		result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
+		totalAmount += thisAmount;
+	}
+	result += `총액: ${format(totalAmount / 100)}\n`;
+	result += `적립포인트: ${format(volumeCredits)}\n`;
+	return result;
+}
+
+// 현재 나쁜냄새가 나는 부분
+// 1 switch문, 2. if comedy
+console.log(statement(invoices[0], plays));
