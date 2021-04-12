d101dcf7de5f31cf199eb27cbd71080545015332
sinholee <dltlsgh5@gmail.com>
2021-04-12 19:42:46 +0900

    다형성을 이용하여 분기 위임하기 도중

--- videoStore/model/createStatementData.js
+++ videoStore/model/createStatementData.js
@@ -1,5 +1,45 @@
 // model or store
 
+class PerformanceCalculator {
+	constructor(aPerformance, aPlay) {
+		this.performance = aPerformance;
+		this.play = aPlay;
+	}
+	get volumeCredits() {
+		let result = 0;
+		// 코메디 장르일 경우 5명 마다 추가 포인트를 적립해준다.
+		if ("comedy" === this.play.type) {
+			result += Math.floor(this.performance.audience / 5);
+		}
+		// 포인트를 적립한다.? 30명 이상이면 포인트를 준다.
+		result += Math.max(this.performance.audience - 30, 0);
+		return result;
+	}
+
+	get amount() {
+		let result = 0;
+		switch (this.play.type) {
+			case "tragedy":
+				result = 40000;
+				if (this.performance.audience > 30) {
+					result += 1000 * (this.performance.audience - 30);
+				}
+				break;
+			case "comedy":
+				result = 30000;
+				if (this.performance.audience > 20) {
+					result += 10000 + 500 * (this.performance.audience - 20);
+				}
+				result += 300 * this.performance.audience;
+				break;
+			default:
+				throw new Error(`알수없는 장르 ${this.play.type}`);
+				break;
+		}
+		return result;
+	}
+}
+
 /**
  * @returns {object} {customer:string, performances:Array, totalAmount:number, totalVolumeCredits:number}
  *
@@ -15,10 +55,11 @@ export default function createStatementData(invoice, plays) {
 
 	// model 변경 혹은 시리얼라이징!
 	function enrichPerformance(aPerformance) {
+		const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
 		const result = Object.assign({}, aPerformance);
-		result.play = playFor(result);
-		result.amount = amountFor(result);
-		result.volumeCredits = volumeCreditsFor(result);
+		result.play = calculator.play;
+		result.amount = calculator.amount;
+		result.volumeCredits = calculator.volumeCredits;
 
 		return result;
 	}
@@ -33,38 +74,11 @@ export default function createStatementData(invoice, plays) {
 	function playFor(aPerformance) {
 		return plays[aPerformance.playID];
 	}
-
 	function amountFor(aPerformance) {
-		let result = 0;
-		switch (aPerformance.play.type) {
-			case "tragedy":
-				result = 40000;
-				if (aPerformance.audience > 30) {
-					result += 1000 * (aPerformance.audience - 30);
-				}
-				break;
-			case "comedy":
-				result = 30000;
-				if (aPerformance.audience > 20) {
-					result += 10000 + 500 * (aPerformance.audience - 20);
-				}
-				result += 300 * aPerformance.audience;
-				break;
-			default:
-				throw new Error(`알수없는 장르 ${playFor(aPerformance).type}`);
-				break;
-		}
-		return result;
+		return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
 	}
 
 	function volumeCreditsFor(aPerformance) {
-		let result = 0;
-		// 코메디 장르일 경우 5명 마다 추가 포인트를 적립해준다.
-		if ("comedy" === aPerformance.play.type) {
-			result += Math.floor(aPerformance.audience / 5);
-		}
-		// 포인트를 적립한다.? 30명 이상이면 포인트를 준다.
-		result += Math.max(aPerformance.audience - 30, 0);
-		return result;
+		return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
 	}
 }
