3207388a3cfc5a61d8af872515279d0c4c02151a
sinholee <dltlsgh5@gmail.com>
2021-04-30 17:43:26 +0900

    chapter7 simpleRecord

---
+++ chapter7/encapsulateRecord/simpleRecord.spec.js
@@ -0,0 +1,27 @@
+import {getOrganization} from "./simpleRecord";
+
+// 사용처
+describe("simpleRecord to encapsulated object", () => {
+	it("read example name", () => {
+		let result = "";
+		result += `<h1>${getOrganization().name}</h1>`;
+		expect(result).toBe("<h1>애크미 구스베리</h1>");
+	});
+	it("read example country", () => {
+		let result = "";
+		result += `<h1>${getOrganization().country}</h1>`;
+		expect(result).toBe("<h1>GB</h1>");
+	});
+	it("write example name", () => {
+		let result = "";
+		getOrganization().name = "sinho";
+		result += `<h1>${getOrganization().name}</h1>`;
+		expect(result).toBe("<h1>sinho</h1>");
+	});
+	it("write example country", () => {
+		let result = "";
+		getOrganization().country = "suwon";
+		result += `<h1>${getOrganization().country}</h1>`;
+		expect(result).toBe("<h1>suwon</h1>");
+	});
+});
