cc020fe9103416944373ce49bc74e1d87e42cda0
siny <siny@nextunicorn.kr>
2022-04-25 20:40:09 +0900

    [REFACTOR] 8-2 done

--- src/chapter8/8-2/index.ts
+++ src/chapter8/8-2/index.ts
@@ -1,40 +1,45 @@
+// 변경 목적
+// discountRate를 구하는 로직을 coustomContract 클래스 내부로 옮기려고 한다.
+
 export default class Customer {
     private readonly _name: string
-    private _discountRate: number
     private readonly _contract: CustomContract
 
     constructor(name: string, discountRate: number) {
         this._name = name
-        this._discountRate = discountRate
-        this._contract = new CustomContract(new Date())
+        this._contract = new CustomContract(new Date(),discountRate)
     }
 
     get discountRate() {
-        return this._discountRate
+        return this._contract.discountRate
     }
 
+    private _setDiscountRate(aNumber:number){
+       this._contract.discountRate = aNumber
+    }
     becomePreferred() {
-        this._discountRate += 0.03
+        this._setDiscountRate(this.discountRate + 0.03)
+
     }
 
     applyDiscount(amount: number) {
-        return amount - (amount * this._discountRate)
+        return amount - (amount * this.discountRate)
     }
 }
+
 
 class CustomContract {
     private readonly startDate: Date
-
-    constructor(startDate: Date) {
+    private _discountRate:number
+    constructor(startDate: Date,discountRate:number) {
         this.startDate = startDate
+        this._discountRate = discountRate
+    }
+    get discountRate(){
+        return this._discountRate
+    }
+    set discountRate(aNum){
+        this._discountRate = aNum
     }
 }
 
-function main() {
-    const c = new Customer('시니', 0.1)
-    console.log(c.applyDiscount(100))
-    c.becomePreferred()
-    console.log(c.applyDiscount(100))
-}
-
-main()
